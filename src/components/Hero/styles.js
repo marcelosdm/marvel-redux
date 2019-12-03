6a6e05e71feb68ad98e0f6a5ff9c9e77a31643d8
marcelosdm <marcelosdm@gmail.com>
2019-12-03 08:12:12 -0300

    Add styles

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   align-items: center;
@@ -11,13 +11,21 @@ export const HeroBox = styled.li`
   justify-content: center;
   list-style-type: none;
   margin: 1px;
+  max-width: 10em;
   padding: 1.5em 0.5em;
+  transition: 0.2s ease;
 `;
 
 export const Picture = styled.img`
+  border-top-left-radius: 15px;
   height: 10em;
   overflow: hidden;
-  width: 10em;
+  &:hover {
+    background-color: #000;
+    border: solid #000;
+    opacity: 0.5;
+    /* border-radius: 30px; */
+  }
 `;
 
 export const Name = styled.h3`
@@ -25,10 +33,11 @@ export const Name = styled.h3`
   background-color: #2e2e2e;
   color: #e8e8e8;
   display: flex;
-  height: 3em;
+  height: 4em;
   justify-content: center;
   flex-wrap: wrap;
   font-size: 15px;
+  margin-bottom: 0.3em;
   text-align: center;
   width: 100%;
   word-wrap: break-word;
